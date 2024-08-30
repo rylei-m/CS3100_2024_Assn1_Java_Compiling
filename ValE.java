a34fb79dda4bc75716c021798fb244079a5c5a07
Rylei Mindrum <A02352206@aggies.usu.edu>
2024-08-29 22:36:42 -0600

    ValE w/ Taylor Series

--- ValE.java
+++ ValE.java
@@ -1,4 +1,12 @@
-package PACKAGE_NAME;
+public class ValE  {
+    public static double CalcE(int n) {
+        double e = 1.0;
+        double factorial = 1.0;
 
-public class ValE {
+        for (int i = 1; i <= n; i++) {
+            factorial *= i;
+            e += 1.0 / factorial;
+        }
+        return e;
+    }
 }
