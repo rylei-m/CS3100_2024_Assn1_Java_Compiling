cd5a7486624e7cac02d6ba0d4523448fff5c8ce4
Rylei Mindrum <A02352206@aggies.usu.edu>
2024-08-31 14:04:23 -0600

    Fib in Main

--- Assn1.java
+++ Assn1.java
@@ -1,7 +1,16 @@
 
 public class Assn1 {
     // TODO: Implement Fibonacci
-
+    public static int fibonacci(int n) {
+        int n1 = 0, n2 = 1, n3;
+        for (int i = 2; i <= n; i++) {
+        n3 = n1 + n2;
+        n1 = n2;
+        n2 = n3;
+    }
+    return n2;
+    }
+    
     // TODO: Implement Factorial with BigNumber
 
     //TODO: Compute Val of E with Taylor Series
