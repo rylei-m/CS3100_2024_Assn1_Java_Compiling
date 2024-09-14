50104c0ed1e5ca10ebd6637d47fa0b8b9ba40804
Rylei Mindrum <rymindrum@gmail.com>
2024-09-13 20:51:52 -0600

    ud

--- src/Un/BigInteger.java
+++
@@ -1,9 +0,0 @@
-public class BigInteger {
-    public static BigInteger factorial(int n) {
-        BigInteger result = BigInteger.ONE;
-        for (int i = 2; i <= n; i++) {
-            result = result.multiply(BigInteger.valueOf(i));
-        }
-        return result;
-    }
-}
